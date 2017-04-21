fcb83bc3a34db328705ebba507d0ae48a37d25a5
DenOn79 <denonufriiev@gmail.com>
2017-04-21 17:48:45 +0300

    modified test3

--- test3.rb
+++ test3.rb
@@ -1,5 +1,5 @@
 
-#=begin
+=begin
 power_array = -> (power, array_size) do 
     1.upto(Float::INFINITY).lazy.map { |x| x**power }.first(array_size) 
 end
@@ -10,14 +10,15 @@ print power_array.(2 , 10)   #[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 puts
 print power_array.(3, 5)     #[1, 8, 27, 64, 125]
 puts
-#=end
+=end
 
 #=begin
 require 'mathn'
-#array_size = gets
+array_size = gets.to_i
 palprime_array = -> (array_size) do
-    2.upto(Float::INFINITY).lazy.map { |x| x.next }.first(array_size)
+    2.upto(Float::INFINITY).lazy.map { |x| (2 .. x).select{|y|x%y==0} }.first(array_size)
 end
 
-puts palprime_array.(5)
+print palprime_array.(array_size)
+puts
 #=end
